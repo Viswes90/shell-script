ab2c558460b6e7a8548a09292e5e277f768d534a
Viswes90 <viswam.kdp@gmail.com>
2025-01-13 16:09:31 +0530

    added variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rupali
+PERSON2=Viswa
+
+echo "$PERSON1 : Hello $PERSON2, How are you ?"
+echo "$PERSON2 : Hey $PERSON1, I am good"
+echo "$PERSON2 : How about you ?"
+echo "$PERSON1 : I am doing good."

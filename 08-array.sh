472ba8c5165a984e5c2025d1f8898da758d2b9e4
Viswes90 <viswam.kdp@gmail.com>
2025-01-14 12:21:01 +0530

    array

---
+++ 08-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Balu","OG","Kushi")
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[2]}"
+echo "Third movie is ${MOVIES[1]}"
+
+echo "Number of movies ${MOVIES[@]}"

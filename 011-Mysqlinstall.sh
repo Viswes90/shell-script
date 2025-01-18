e3976bede97d23e3ba5623d546e7697e0482c875
Viswes90 <viswam.kdp@gmail.com>
2025-01-18 13:35:17 +0530

    mysqlscript

---
+++ 011-Mysqlinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+echo "${USERID}"
+
+if [ $USERID -ne 0]
+    then 
+        echo "Error: To execute this script SUDO previlige is required"
+        exit 1
+    else
+        echo "You are a ROOT User"
+fi

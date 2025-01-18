556583e8b8bf161d6bdfd16ad62910d9d2a3727d
Viswes90 <viswam.kdp@gmail.com>
2025-01-18 12:51:27 +0530

    conditions

---
+++ 010-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER1=S1
+
+# -gt, -lt, -eq, -le, -ge
+
+if [ $NUMBER1 -gt 100 ]
+    then
+        echo "Given Number $NUMBER1 is greater than 100"
+    else
+        echo "Given Number $NUMBER1 is less than 100"
+fi

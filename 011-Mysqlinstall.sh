ce8a1ddee1e62d224c089afade657f3d59a3f35d
Viswes90 <viswam.kdp@gmail.com>
2025-01-18 14:01:41 +0530

    mysqlscript

--- 011-Mysqlinstall.sh
+++ 011-Mysqlinstall.sh
@@ -10,3 +10,21 @@ if [ $USERID -ne 0 ]
     else
         echo "You are a ROOT User. Executing the Script..."
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+    then # mysql is not installed
+        echo "MySQL is Installing ..."
+        dnf install mysql -y
+        if [ $? -ne 0 ]
+            then
+                echo "Installation of MySQL ... FAILED"
+                exit 1
+            else
+                echo "Installtion of MySQL ... SUCCESS"
+            fi
+    else
+        echo "MySQL is already ... INSTALLED"
+fi
+

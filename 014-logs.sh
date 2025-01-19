bb9e9103cd2abdf9562c288749a69e95d1356bcf
Viswes90 <viswam.kdp@gmail.com>
2025-01-19 11:35:55 +0530

    logs

--- 014-logs.sh
+++ 014-logs.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+echo "Script Start Time : $TIMESTAMP" &>>$LOG_FILE_NAME
+
 USERID=$(id -u)
 echo "${USERID}"
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/viswa_logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$( date +%Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE_$TIMESTAMP.log"
 
 VALIDATE()
 {
@@ -18,6 +26,8 @@ VALIDATE()
     fi
 }
 
+
+
 if [ $USERID -ne 0 ]
     then 
         echo -e "$R Error: To execute this script SUDO previlige is required $N"
@@ -26,23 +36,23 @@ if [ $USERID -ne 0 ]
         echo -e "$G You are a ROOT User. Executing the Script... $N"
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
     then # mysql is not installed
         echo -e "MySQL is Installing ..."
-        dnf install mysql -y
+        dnf install mysql -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing MySQL"
     else
         echo -e "MySQL is already ... $Y INSTALLED $N"
 fi
 
-dnf list installed git
+dnf list installed git &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
     then # git is not installed
         echo -e "Git is Installing ..."
-        dnf install git -y
+        dnf install git -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing GIT"
     else 
         echo -e "GIT is already ... $Y INSTALLED $N"
